d9a191310487e7c0d8b2e12d2b618d4ec5298be5
saifDeveloper <saikhsaifali@gmail.com>
2017-07-20 14:21:29 +0530

    Router implemented

---
+++ routes/routes.js
@@ -0,0 +1,93 @@
+const express = require('express');
+const router = express.Router();
+
+let Articles = require('../models/article');
+
+router.get('/add',function(req,res){
+    res.render('add_article',{
+        title: 'Add article'
+    });
+});
+
+
+
+router.get('/edit/:id',function (req,res) {
+    Articles.findById(req.params.id,function (err,article) {
+        res.render('edit_article',{
+            title:'Edit Article',
+            article: article
+        });
+    });
+});
+
+router.post('/add',function(req,res) {
+
+    req.checkBody('title','Title is required').notEmpty();
+    req.checkBody('author','Author is required').notEmpty();
+    req.checkBody('body','Body is required').notEmpty();
+
+
+
+    let errors = req.validationErrors();
+
+    if(errors){
+        res.render('add_article',{
+            title: 'Add article',
+            errors:errors
+        });
+    } else {
+        let article = new Articles();
+        article.title = req.body.title;
+        article.author = req.body.author;
+        article.body = req.body.body;
+
+        article.save(function(err){
+            if(err){
+                console.log(err);
+
+            } else{
+                req.flash('success','Article Added');
+                res.redirect('/');
+            }
+        })
+    }
+});
+
+router.post('/edit/:id',function (req,res) {
+    let article = {};
+    article.title = req.body.title;
+    article.author = req.body.author;
+    article.body = req.body.body;
+
+    let query = {_id:req.params.id};
+
+    Articles.update(query,article,function(err){
+        if(err){
+            console.log(err);
+
+        } else{
+            req.flash('success','Article Updated');
+            res.redirect('/');
+        }
+    })
+});
+
+router.delete('/:id',function (req,res) {
+    let query ={_id:req.params.id};
+
+    Articles.remove(query,function (err) {
+        if(err){
+            console.log(err);
+        }
+        res.send('Success');
+    });
+});
+
+router.get('/:id',function (req,res) {
+    Articles.findById(req.params.id,function (err,article) {
+        res.render('article',{
+            article: article
+        });
+    });
+});
+module.exports = router;
