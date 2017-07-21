4aee9a4dea24f146685a9b2416e69a7d2e645570
saifDeveloper <saikhsaifali@gmail.com>
2017-07-21 22:54:32 +0530

    user registration done

--- app.js
+++ app.js
@@ -81,6 +81,8 @@ app.get('/',function(req,res){
 let routes = require('./routes/routes');
 app.use('/articles',routes);
 
+let users = require('./routes/user');
+app.use('/users',users);
 
 app.listen(3000,function(){
     console.log('Server started on port 3000...');
